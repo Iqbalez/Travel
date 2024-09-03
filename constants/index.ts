3ea0105279970f2fcb50f0cbc659d18968ef70b8
Iqbalez <iqbalezedin5@gmail.com>
2024-09-03 02:05:01 -0700

    initial commit

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_hilink_work', label: 'How sultan Work?' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Tropical Paradises',
+    icon: '/map.svg',
+    variant: 'green',
+    description:
+      'Explore stunning beaches and crystal-clear waters in destinations like the Zanzibar, Maldives, Bali, and the Caribbean.',
+  },
+  {
+    title: 'Adventure Awaits',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      "Embark on thrilling adventures in the Rockies, the Amazon Rainforest, or the Swiss Alps.",
+  },
+  {
+    title: 'Cultural Journeys',
+    icon: '/tech.svg',
+    variant: 'green',
+    description:
+      'Immerse yourself in rich histories and vibrant cultures in cities like South africa, Paris, Tokyo, and Rome.',
+  },
+  {
+    title: 'Many new locations every month',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Lots of new locations every month, because we have a worldwide community of travelers who share their best experiences with traveling.',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About Sultan Travel',
+      'Press Releases',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Travel to Dubai', 'Travel to South Africa', 'Travel to Zimbabwa'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '+251900000000' },
+    { label: 'Email Officer', value: 'sultanTravel@gmail.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
